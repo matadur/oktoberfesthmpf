ee3fb150362db4b0c3569aad6805925ab15e7149
Matt Durr <onedurr@gmail.com>
2013-08-02 13:05:46 -0400

    added smooth scroll to section from banner click

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
+// smooth scroll to position in page on banner link click
+$("#main-nav ul li a[href^='#']").on('click', function(e) {
+   e.preventDefault();
+   $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
 
+   // edit: Opera and IE requires the "html" elm. animated
+});

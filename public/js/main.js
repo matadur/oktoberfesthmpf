8b7c3c7a67a32ab1962c718d9a33e0300ebfdbac
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2013-08-07 00:13:26 -0400

    Transition images on hover (need to improve consistency of image sizes)

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,20 @@
-// smooth scroll to position in page on banner link click
-$("#main-nav ul li a[href^='#']").on('click', function(e) {
-   e.preventDefault();
-   $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
 
-   // edit: Opera and IE requires the "html" elm. animated
+
+$(function () {
+	var navLinks = $('#main-nav ul li a[href^="#"]'),
+		body = $('body');
+
+	// smooth scroll to position in page on banner link click
+	navLinks.on('click', function (e) {
+	   e.preventDefault();
+	   $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
+	
+	   // edit: Opera and IE requires the "html" elm. animated
+	});
+	
+	navLinks.hover(function (e) {
+		body.toggleClass(e.currentTarget.hash.replace('#', ''), true);
+	}, function (e) {
+		body.toggleClass(e.currentTarget.hash.replace('#', ''), false);
+	});
 });

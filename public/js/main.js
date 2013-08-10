016bac3c4661b654e1dd6fbbb1dc1f130b34f428
Matt Durr <onedurr@gmail.com>
2013-08-10 12:49:05 -0400

    preload background images

--- public/js/main.js
+++ public/js/main.js
@@ -13,4 +13,19 @@ $(function () {
 	}, function (e) {
 		body.toggleClass(e.currentTarget.hash.replace('#', ''), false);
 	});
+	
+  var images = [
+    	'../img/glasses.jpg',
+      '../img/music.jpg',
+      '../img/bikes.jpg',
+      '../img/vendors2c.jpg'
+    ];
+			
+  $.preload(images, 1, function(last){
+	  	if (last) {
+	  		/* console.log('images loaded'); */
+	  	}
+	  });
+			
 });
+

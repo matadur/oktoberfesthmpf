3310b43741b2cefd0e5e7054d73e28f03bb20b80
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2013-08-24 16:05:06 -0400

    Update background on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,22 @@ $(function () {
 
 	// smooth scroll to position in page on banner link click
 	navLinks.on('click', function (e) {
-	   e.preventDefault();
-	   $('html, body').animate({ scrollTop: $(this.hash).offset().top - 150 }, 300);
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top - 150 }, 300);
 	});
+	
+	body.on('activate.bs.scrollspy', function (e) {
+	    var currentTarget = $(e.target).find('a');
+        body.removeClass();
+        body.toggleClass(currentTarget.attr('href').replace('#', '') + '-current', true);
+    });
 	
 	navLinks.hover(function (e) {
 		body.toggleClass(e.currentTarget.hash.replace('#', ''), true);
+		body.toggleClass('hover', true);
 	}, function (e) {
 		body.toggleClass(e.currentTarget.hash.replace('#', ''), false);
+		body.toggleClass('hover', false);
 	});
 });
 

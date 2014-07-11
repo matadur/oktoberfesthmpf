51f1cdd02b0bcd92b7c7c22f08b42a13f882898d
Matt Durr <onedurr@gmail.com>
2014-07-11 11:27:20 -0400

    node changes, instructions updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname + "/public", { maxAge: 86400000 })
-).listen(8080);
+var connect = require('connect'); 
+var serveStatic = require('serve-static'); 
+var app = connect(); 
+app.use(serveStatic(__dirname + "/public")); 
+app.listen(5000);
